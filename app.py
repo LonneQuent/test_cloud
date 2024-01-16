310a35c1a74d2cf37b1e7c41000da01e7adaf7ee
LonneQuent <lonnequentin@gmail.com>
2024-01-16 10:35:20 +0100

    maj

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
 
-# Init flash APP
+# Initialize Flask App
 app = Flask(__name__)
 
-# main
-@app.route( '/', methods=['GET'])
-def main(enterpriseId):
+# Main route
+@app.route('/', methods=['GET'])
+def main_route():
     return 'Hello World'
 
+# Hetic route
+@app.route('/hetic', methods=['GET'])
+def hetic_route():
+    return 'Hello Hetic'
+
 if __name__ == '__main__':
-    app.run("0.0.0.0", port=80)
+    app.run("0.0.0.0", port=9999)
